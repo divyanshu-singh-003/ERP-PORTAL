d892bb3fa3c7fb4930f66d59fce7fe65ccf70321
Divyanshu Singh <divyanshusingh12082003@gmail.com>
2024-04-06 16:29:29 +0530

    Layout done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,51 @@
 import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate ,Outlet} from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import { Toaster } from 'react-hot-toast';
 import { useAuthContext } from "./context/AuthContext";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Users from './pages/users/Users';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Menu from './components/menu/Menu';
+import "./styles/global.scss"
+
+// const queryClient = new QueryClient();
 
 
 function App() {
   const { authUser } = useAuthContext();
+
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet/>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+
   return (
     <>
       
           <Routes>
-          <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+          <Route path='/' element={authUser ? <Layout /> : <Navigate to={"/login"} />}>
+            <Route index element ={<Home />}/>
+          </Route>
           <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
           </Routes>
           <Toaster />
-          
-          
-        
-        
-      
     </>
   );
 }
---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Made by Divyanshu Singh</span>
+      <span>IIIT Pune Student Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
